fd2cf80abd5825c4dca5035241d6591a78998e07
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-05-31 23:55:03 -0400

    add money bill

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script>
 	import { goto } from '$app/navigation';
 	import Button from '$lib/components/Button.svelte';
+	import FaMoneyBill from '$lib/icons/FaMoneyBill.svelte';
 </script>
 
-<h1 class="animate-pulse text-6xl font-bold text-blurple-1">Discord Casino</h1>
+<h1 class="animate-pulse text-6xl font-bold text-blurple-1 flex items-end gap-8">
+	<FaMoneyBill class="w-12 h-12" />
+	Discord Casino
+	<FaMoneyBill class="w-12 h-12" />
+</h1>
 <p class="text-lg text-white/80">Become a professional gambler today and be rich!</p>
 
 <div class="max-w-lg w-full mt-4 flex flex-col gap-2">
